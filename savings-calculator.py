1b4b7e8d136972eb9c018c7601b7a38ce0ac7b17
jkwebfolio <busyblogger123@gmail.com>
2025-09-27 22:07:25 +1000

    Add files via upload

--- savings-calculator.py
+++ savings-calculator.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 savings = 100
 new_savings = 40
 
@@ -7,3 +9,14 @@ print("Savings = ",total_savings)
 
 # Print the type of total_savings
 print(type(total_savings))
+
+# Add income and expense for this month
+income = [["job",1000],
+          ["hustle1",200],
+          ["tutoring",300]]
+expense = [["petrol",50],
+           ["takeaway",50]]
+
+
+print(income)
+print(expense)
